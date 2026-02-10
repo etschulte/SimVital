f1583a34e83f22c2eacc64be7942a8e2548a0d20
etschulte <etschulte04@gmail.com>
2026-02-10 15:20:35 -0500

    completed user story #25

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <QApplication>
+#include <QLabel>
 
-int main() {
-    std::cout << "This will be SimVital" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QLabel label("SimVital");
+    label.setAlignment(Qt::AlignCenter);
+    label.resize(400, 300);
+    label.show();
+
+    return app.exec();
 }
