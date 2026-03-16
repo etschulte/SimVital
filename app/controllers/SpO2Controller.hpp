d9e19e11b910177d176a0d906b96b8f8b63e4c14
etschulte <etschulte04@gmail.com>
2026-03-16 18:30:36 -0400

    created spO2 controller header

--- app/controllers/SpO2Controller.hpp
+++ app/controllers/SpO2Controller.hpp
@@ -6,5 +6,21 @@
 
 class SpO2Controller : public QObject {
     Q_OBJECT
+    Q_PROPERTY(int spO2Val READ getSpO2Val NOTIFY spO2ValChanged)
+
+private:
+    int latestSpO2Val;
+    SpO2Generator* generatorPtr;
+    
+public:
+    SpO2Controller(SpO2Generator* generatorPtr, QObject* parent = nullptr);
+
+    int getSpO2Val() const;
+
+public slots:
+    void updateSpO2Val(int spO2Val);
+
+signals:
+    
 
 };
